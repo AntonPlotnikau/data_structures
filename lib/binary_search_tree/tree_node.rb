6bed1a91e1bd2ab7ef5c79e6391d4a405b196a5f
AntonPlotnikau <37152691+AntonPlotnikau@users.noreply.github.com>
2018-12-07 18:29:22 +0300

    Update tree_node.rb

--- lib/binary_search_tree/tree_node.rb
+++ lib/binary_search_tree/tree_node.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
 
-# class For Node in BST
-class TreeNode
-  attr_accessor :left, :value, :right
-  def initialize(value)
-    @value = value
-    @right = nil
-    @left = nil
-  end
-end
